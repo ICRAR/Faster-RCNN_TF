1f8161dbb78d508c037a594a3dfd9562f27436f0
Chen Wu <chen.wu@icrar.org>
2017-06-22 12:44:04 +0800

    add constructor to __init__

--- lib/datasets/__init__.py
+++ lib/datasets/__init__.py
@@ -13,6 +13,7 @@ from .kitti import kitti
 from .kitti_tracking import kitti_tracking
 from .nissan import nissan
 from .nthu import nthu
+from .rgz import rgz
 from . import factory
 
 import os.path as osp
