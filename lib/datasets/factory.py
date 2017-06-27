b6563f9b33bffb41b0f2c7c24800e2e5abed6911
Chen Wu <chen.wu@icrar.org>
2017-06-27 17:58:06 +0800

    add train/testfifth dataset to the factory

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -80,7 +80,8 @@ for split in ['71', '370']:
 for year in ['2017']:
     for split in ['train', 'test', 'trainsecond',
                   'trainthird', 'testthird', 'trainthirdsmall',
-                  'trainfourth', 'testfourth', 'testthirdsmall']:
+                  'trainfourth', 'testfourth', 'testthirdsmall',
+                  'trainfifth', 'testfifth']:
         name = 'rgz_{}_{}'.format(year, split)
         print name
         __sets[name] = (lambda split=split, year=year:
