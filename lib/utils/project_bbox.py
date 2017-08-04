1026b639dd273550d78338a1aac07e3d441ba28f
Chen Wu <chen.wu@icrar.org>
2017-08-05 00:16:51 +0800

    normalise gt bbox for affine transformation

--- lib/utils/project_bbox.py
+++ lib/utils/project_bbox.py
@@ -22,8 +22,8 @@ def project_bbox(gt_boxes, theta, im_size=(600, 600)):
     2. for each box,
     """
     #return gt_boxes
-    print("*** theta = {0}".format(theta))
-    print("--- gt_boxes = {0}".format(gt_boxes))
+    #print("*** theta = {0}".format(theta))
+    #print("--- gt_boxes = {0}".format(gt_boxes))
     theta = np.reshape(theta, (-1, 3)) # in case it is unflattened
     if (np.sum(theta == identity) == 6):
         return gt_boxes
@@ -32,6 +32,14 @@ def project_bbox(gt_boxes, theta, im_size=(600, 600)):
     cls_lbl = np.reshape(gt_boxes[:, -1], [num_gtb, 1])
 
     boxes = np.transpose(gt_boxes)[0:-1]
+
+    # normalise coordinates for affine transform
+    boxes[(0, 2), :] -= im_size[1] / 2.0 # x
+    boxes[(0, 2), :] /= im_size[1] / 2.0
+
+    boxes[(1, 3), :] -= im_size[0] / 2.0
+    boxes[(1, 3), :] /= im_size[0] / 2.0
+
     #boxes = np.hstack((boxes[0:2, :], boxes[2:4, :])) # shape = (2, num_gtb * 2)
 
     # shape = (2, num_gtb * 4)
@@ -41,7 +49,7 @@ def project_bbox(gt_boxes, theta, im_size=(600, 600)):
 
     new_boxes = np.vstack((boxes,  np.ones((1, num_gtb * 4), dtype=np.float32)))
     new_coord1 = np.dot(theta, new_boxes)
-    print("++++++ new_coord1 = {0}".format(new_coord1))
+    #print("++++++ new_coord1 = {0}".format(new_coord1))
 
     row, col = new_coord1.shape
     x_row = new_coord1[0]
@@ -49,21 +57,26 @@ def project_bbox(gt_boxes, theta, im_size=(600, 600)):
     new_coord = np.zeros([row, col / 2], dtype=np.float32)
     count_error = 0
     for i in range(num_gtb):
-        new_coord[0][i] = max(np.min(x_row[i::num_gtb]), 0)
-        new_coord[0][i + num_gtb] = min(np.max(x_row[i::num_gtb]), im_size[1] - 1)
+        new_coord[0][i] = max(np.min(x_row[i::num_gtb]), -1.0)
+        new_coord[0][i + num_gtb] = min(np.max(x_row[i::num_gtb]), 1.0)
         #assert(new_coord[0][i + num_gtb] > new_coord[0][i])
         if (new_coord[0][i + num_gtb] <= new_coord[0][i]):
             print("{0} <= {1}".format(new_coord[0][i + num_gtb], new_coord[0][i]))
             count_error += 1
-        new_coord[1][i] = max(np.min(y_row[i::num_gtb]), 0)
-        new_coord[1][i + num_gtb] = min(np.max(y_row[i::num_gtb]), im_size[0] - 1)
+        new_coord[1][i] = max(np.min(y_row[i::num_gtb]), -1.0)
+        new_coord[1][i + num_gtb] = min(np.max(y_row[i::num_gtb]), 1.0)
         #assert(new_coord[1][i + num_gtb] > new_coord[1][i])
         if (new_coord[1][i + num_gtb] <= new_coord[1][i]):
             print("{0} <= {1}".format(new_coord[1][i + num_gtb], new_coord[1][i]))
             count_error += 1
 
+    new_coord[0, :] *= im_size[1] / 2.0
+    new_coord[0, :] += im_size[1] / 2.0
+    new_coord[1, :] *= im_size[0] / 2.0
+    new_coord[1, :] += im_size[0] / 2.0
+
     new_cc = np.transpose(np.vstack((new_coord[:, 0:num_gtb], new_coord[:, num_gtb:num_gtb * 2])))
-    print("xxxxx new gt_boxes = {0}".format(new_cc))
+    #print("xxxxx new gt_boxes = {0}".format(new_cc))
 
     if (count_error > 0):
         raise Exception("Assertion Error")
